1d57532c3149100b799e07283cd68d4e40de5905
Olga Beatriz Fernando da Silva <olga.beatriz@etaplus.energy>
2025-04-06 23:28:00 +0200

    test: add basic unit test to validate CI pipeline

--- MatchTracker.Tests/UnitTest1.cs
+++ MatchTracker.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Xunit;
+
 namespace MatchTracker.Tests
 {
     public class UnitTest1
@@ -5,7 +7,15 @@ namespace MatchTracker.Tests
         [Fact]
         public void Test1()
         {
+            // Arrange
+            int a = 2;
+            int b = 3;
 
+            // Act
+            int sum = a + b;
+
+            // Assert
+            Assert.Equal(5, sum);
         }
     }
 }
