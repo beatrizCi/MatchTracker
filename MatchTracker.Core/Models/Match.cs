d9376a51ffd8af996fa25f25df56c65f8c7edf1e
Olga Beatriz Fernando da Silva <olga.beatriz@etaplus.energy>
2025-04-11 01:57:48 +0200

    updating migrations

--- MatchTracker.Core/Models/Match.cs
+++ MatchTracker.Core/Models/Match.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace MatchTracker.Core.Models
 {
     public class Match
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string? TeamA { get; set; }
         public string? TeamB { get; set; }
---
+++ MatchTracker.Infrastructure/Migrations/20250410235536_FixMatchIdGeneration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MatchTracker.Infrastructure.Migrations
+{
+    public partial class FixMatchIdGeneration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
